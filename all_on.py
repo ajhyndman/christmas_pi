3544a7bfe46acc021a334e39e4f13cd3edeaabca
PI <myndman@gmail.com>
2015-12-20 04:18:51 +0000

    a

--- all_on.py
+++ all_on.py
@@ -2,9 +2,8 @@ import RPi.GPIO as GPIO
 import time
 import config
 
-for pin in config.PINS[:1]:
+for pin in config.PINS:
     GPIO.setup(pin, GPIO.OUT)
-    time.sleep(0.5)
     GPIO.output(pin, config.ON)
 
 time.sleep(5)
