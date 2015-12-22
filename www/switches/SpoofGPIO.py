ec55ff2798e2b5d9db9eeb9bd4df6d23b7de636a
Andrew Hyndman <ajhyndman@hotmail.com>
2015-12-21 23:44:23 -0800

    created spoof GPIO module

---
+++ www/switches/SpoofGPIO.py
@@ -0,0 +1,19 @@
+# Spoof RPi.GPIO class for Raspberry Pi GPIO development
+
+HIGH = True
+LOW = False
+
+OUT = 0
+BOARD = 0
+
+def setmode(mode):
+    pass
+
+def setup(id, mode):
+    pass
+
+def output(id, value):
+    pass
+
+def cleanup():
+    pass
