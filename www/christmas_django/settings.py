8a97b4728c2c3c48a01725df74c8348bf91c5d6c
Andrew Hyndman <ajhyndman@hotmail.com>
2015-12-22 16:04:24 -0800

    added some templates and incorprated materialize

--- www/christmas_django/settings.py
+++ www/christmas_django/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'christmas_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
