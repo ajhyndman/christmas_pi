73ff84c69678ffa8ec963e280589237afb689d61
PI <myndman@gmail.com>
2015-12-23 00:07:46 +0000

    updated .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+import librosa
+
 import sys
 import time
 import config
@@ -5,7 +8,7 @@ import config
 # I can't import/install gpio on windows
 pi = None
 
-import pygame
+#import pygame
 #import librosa
 
 class Player:
@@ -31,10 +34,12 @@ class Player:
     def load(self, file):
         sys.stdout.write('Loading\n')
         sys.stdout.flush()
+        '''
         pygame.init()
         pygame.mixer.init()
         pygame.mixer.music.load(file)
-        #self.song = [(1,0.1),(3,0.2),(0,0.3)] * 10
+        '''
+        #
         '''
         y, sr = librosa.load(file)
         tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
@@ -45,6 +50,7 @@ class Player:
         sys.stdout.flush()
 
     def play(self):
+        '''
         pygame.display.set_mode((100, 100))
         pygame.mixer.music.play()
         count = 0
@@ -54,13 +60,26 @@ class Player:
             sys.stdout.flush()
             self.restart_line()
             pygame.time.Clock().tick(10)
+        '''
         #for vol, dur in self.song:
         #    self.set_output(vol)
         #    time.sleep(dur)
         if not self.console:
             pi.cleanup()
 
+def processSong(file):
+    y, sr = librosa.load(file)
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+    # 4. Convert the frame indices of beat events into timestamps
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+    print('Beat times: %s' % beat_times)
+    return 1
+
 if __name__ == "__main__":
     p = Player(console=True)
-    p.load('songs/Witch Hunt.ogg')
+    filename = 'songs/Witch Hunt.mp3'
+    filename = librosa.util.example_audio_file()
+    p.load(filename)
+    processSong(filename)
     p.play()
