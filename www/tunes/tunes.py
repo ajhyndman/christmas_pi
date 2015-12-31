225d710b81bbdb859f0b168bdbbfae08b62c829d
Andrew Hyndman <ajhyndman@hotmail.com>
2015-12-31 02:38:33 -0800

    finished post track for canon (melody)

--- www/tunes/tunes.py
+++ www/tunes/tunes.py
@@ -21,11 +21,13 @@ OLD_REST =      score.OLD_Rest(0.5)
 
 
 # Shorthands for Track Compilation style Notes and Rests
+ES = E = EIGHTH_NOTE = score.Note(0.5)
 QS = Q = QUARTER_NOTE = score.Note(1)
 H = HALF_NOTE = score.Note(2)
 W = WHOLE_NOTE = score.Note(4)
 
 R = QUARTER_REST = score.Rest(1)
+ER = EIGHTH_REST = score.Rest(0.5)
 
 
 
@@ -101,7 +103,8 @@ TUNES = [
             # Posts
             score.Track(0,
                 [
-                    # score.Note(1), score.Rest(1), score.Rest(1), score.Note(1),
+                    ## PAGE 1 ##
+                    # 0:00
                     Q, R, R, Q,   R, R, Q, R,   Q, R, R, Q,   R, R, Q, R,
                     Q, R, R, Q,   R, R, Q, R,   Q, R, R, Q,   R, R, Q, R,
                     
@@ -110,12 +113,58 @@ TUNES = [
                     
                     Q, R, R, Q,   R, R, Q, R,   Q, R, R, Q,   R, R, Q, R,
                     Q, R, R, Q,   R, R, Q, R,   Q, R, R, Q,   R, R, Q, R,
-                ]
+                ] + [
+                    ## PAGE 2 ##
+                    # 0:42
+                    Q, ER, ES, Q, R,   E, ER, ER, E, ER, ER, E, ER,   Q, ER, ES, Q, R,   E, ER, ER, E, ER, ER, E, ER,
+                ] + [
+                    # 0:50
+                    Q, ES, ER, ER, E, R,   E, ER, ER, E, ER, ER, E, ER,   Q, ES, ER, Q, R,   E, ER, ER, E, R, E, ER,
+                ] + ([
+                    # 0:57
+                    Q, R, R, R,   Q, R, R, R,   Q, R, R, R,   Q, R, R, R,
+                ] * 4) + ([
+                    # 1:26
+                    E, E, R, R, R,   E, E, R, R, R,   E, E, R, R, R,   E, E, R, R, R,
+                ] * 4) + ([
+                    # 1:55
+                    E, E, R, E, E, R,   E, E, ER, E, ER, E, R,   E, E, R, E, E, R,  E, E, ER, E, ER, E, R
+                ] * 3) + [
+                    ## PAGE 3 ##
+                    # 2:16
+                    E, E, R, E, E, R,   E, E, ER, E, ER, E, R,   E, E, R, E, E, R,   E, E, ER, E, ER, score.Note(1.5)
+                ] + [
+                    # ~2:25
+                    score.Note(3), QS,   Q, R, R, Q,   R, R, R, R,   R, R, Q, R,
+                    W,   R, R, R, R,   Q, R, H,   W,
+                    score.Note(3), QS,   Q, R, R, Q,   R, R, R, R,   R, R, Q, R,
+                    W,   R, R, R, R,   Q, R, H,   W, 
+                ] + [
+                    ## PAGE 4 ##
+                    # 2:53
+                    Q, ER, E, Q, R,   E, ER, ER, E, R, E, ER,   Q, ER, E, Q, R,   E, ER, ER, E, R, E, ER,
+                ] + [
+                    # 3:00
+                    Q, E, ER, Q, R,   E, ER, ER, E, R, E, ER,   Q, E, ER, Q, R,   E, ER, ER, E, R, E, ER,
+                ] + [
+                    # 3:08
+                    Q, ER, E, Q, R,   E, ER, ER, E, R, E, ER,   Q, ER, E, Q, R,  E, ER, ER, E, R, E, ER
+                ] + [
+                    # 3:14
+                    Q, E, ER, Q, R,   E, ER, ER, E, R, E, ER
+                ] + [
+                    # 3:18
+                    Q, E, ER, Q, R,   E, ER, ER, E, R,   score.Note(6)
+                ] + ([
+                    # Ask Violet: 16th notes?
+                    score.Note(1/3), score.Note(1/3), score.Note(1/3),
+                ] * 16)
             ),
             
             # Bush
             score.Track(1,
                 [
+                    # 0:00
                     R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,
                     R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,
                     
@@ -124,6 +173,9 @@ TUNES = [
                     
                     R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,
                     R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,   R, Q, R, Q,
+                ] + [
+                    # 0:42
+                    
                 ]
             ),
             
