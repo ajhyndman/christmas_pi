95f5f419bbbb572b0f90434370ed24195d2f8c3f
Andrew Hyndman <ajhyndman@hotmail.com>
2015-12-30 17:51:08 -0800

    added pachelbel's canon (bassline)

--- www/tunes/score.py
+++ www/tunes/score.py
@@ -9,20 +9,36 @@ BEAT_DURATION = 0.1
 
 
 class Note:
+
+    # the percentage of one bar that the note should last.
     delay = 0
     # switch = gpio.Switch(0)
-    switch = None
+    switches = []
+    # length of a bar in seconds
+    beat = 1
     
-    def __init__(self, delay, switch):
+    def __init__(self, delay, switches):
         self.delay = delay
-        self.switch = switch
+        self.switches = switches
     
-    def play(self):
+    def add_switch(self, switch):
+        self.switches.append(switch)
+    
+    # Plays a note.
+    #
+    # @param beat:
+    #   the length of the bar the note is scaled against.
+    def play(self, beat):
+        if (beat != None):
+            self.beat = beat
+        
         try:
-            self.switch.on()
-            time.sleep(self.delay*0.75)
-            self.switch.off()
-            time.sleep(self.delay*0.25)
+            for switch in switches:
+                self.switch.on()
+            time.sleep(self.delay*0.75*self.beat)
+            for switch in switches:
+                self.switch.off()
+            time.sleep(self.delay*0.25*self.beat)
         except:
             print('Notes must be played with an object of type "Switch".')
 
--- www/tunes/tunes.py
+++ www/tunes/tunes.py
@@ -5,9 +5,22 @@ import tracks.jinglebells as jinglebells
 
 
 
+A = Zero = score.Note(0.25 [gpio.SWITCHES[0], gpio.SWITCHES[1], gpio.SWITCHES[2])
+B = Zero = score.Note(0.25 [gpio.SWITCHES[1], gpio.SWITCHES[2])
+D = Zero = score.Note(0.25 [gpio.SWITCHES[0], gpio.SWITCHES[1])
+C = Zero = score.Note(0.25 [gpio.SWITCHES[0], gpio.SWITCHES[2])
+E = Zero = score.Note(0.25 [gpio.SWITCHES[2])
+F = Zero = score.Note(0.25 [gpio.SWITCHES[1])
+G = Zero = score.Note(0.25 [gpio.SWITCHES[0])
+REST = score.Rest(0.25)
+
+
+
+
 
 class Tune:
     name = 'default'
+    beat = 1 #length of a bar in seconds
     score = []
     
     def __init__(self, name, score):
@@ -19,7 +32,7 @@ class Tune:
     
     def play(self):
         for note in self.score:
-            note.play()
+            note.play(self.beat)
 
 
 
@@ -28,35 +41,42 @@ class Tune:
 TUNES = [
     Tune('Mary Had a Little Lamb',
         [
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[2]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.5, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.5, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.5, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[2]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(0.25, gpio.SWITCHES[0]),
-            score.Note(0.25, gpio.SWITCHES[1]),
-            score.Note(1, gpio.SWITCHES[2]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[2]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.5, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.5, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.5, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[2]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(0.25, [gpio.SWITCHES[0]]),
+            score.Note(0.25, [gpio.SWITCHES[1]]),
+            score.Note(1, [gpio.SWITCHES[2]]),
         ]),
 
     # truncates to the first 20 beats (~10s)
     Tune('Jingle Bells',
-        score.build_score_from_beats(jinglebells.harmonic, gpio.SWITCHES[2])[:30])
+        score.build_score_from_beats(jinglebells.harmonic, gpio.SWITCHES[2])[:30]),
+        
+    Tune('Pachelbel\'s Canon', [
+        D, A, B, F, G, D, G, A,
+        D, A, B, F, G, D, G, A,
+        D, A, B, F, G, D, G, A,
+        D, A, B, F, G, D, G, A,
+    ])
 ]
